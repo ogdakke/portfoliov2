7188b76c7d4ff97594c235c947e3f15161bdc755
ogdakke <daniel.wargh1@gmail.com>
2022-12-16 16:27:13 +0200

    faviconit sun muut

--- components/meta.js
+++ components/meta.js
@@ -13,20 +13,16 @@ export default function Meta() {
         rel="icon"
         type="image/png"
         sizes="32x32"
-        href="/favicon/favicon-32x32.png"
+        href="/favicon/favicon@32px.png"
       />
       <link
         rel="icon"
         type="image/png"
         sizes="16x16"
-        href="/favicon/favicon-16x16.png"
+        href="/favicon/favicon@16px.png"
       />
       <link rel="manifest" href="/favicon/site.webmanifest" />
-      <link
-        rel="mask-icon"
-        href="/favicon/safari-pinned-tab.svg"
-        color="#000000"
-      />
+    
       <link rel="shortcut icon" href="/favicon/favicon.ico" />
       <meta name="msapplication-TileColor" content="#000000" />
       <meta name="msapplication-config" content="/favicon/browserconfig.xml" />
@@ -34,7 +30,7 @@ export default function Meta() {
       <link rel="alternate" type="application/rss+xml" href="/feed.xml" />
       <meta
         name="description"
-        content= 'A personal developer portfolio in the form of an website for DW.'
+        content= "A personal developer portfolio in the form of an website for DEW. I develop websites and webapps for different purposes, but always with quality in mind."
       />
       <meta property="og:image" content={HOME_OG_IMAGE_URL} key="ogImage" />
     </Head>
