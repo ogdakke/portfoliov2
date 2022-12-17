5fd04128565900752928fed873ed4c19c24e59d6
ogdakke <daniel.wargh1@gmail.com>
2022-12-17 21:44:22 +0200

    intron ja navbarin värkkäystä

--- components/intro.js
+++ components/intro.js
@@ -4,7 +4,7 @@ import Link from 'next/link'
 export default function Intro() {
   return (
     <section className="flex-col md:flex-row flex justify-items-center md:justify-between my-5 md:mb-12">
-      <Link href={"/"} className="text-4xl md:text-6xl font-bold tracking-tighter leading-tight">
+      <Link href={"/"} className="text-4xl md:text-5xl lg:text-6xl font-bold tracking-tighter leading-tight">
         Hi, I'm Daniel E. W.
       </Link>
       <h4 className="max-w-xs md:max-w-sm text-left text-lg mt-5 md:pl-8">
--- components/navbar.js
+++ components/navbar.js
@@ -7,9 +7,9 @@ export default function Navbar() {
     <div className="flex justify-center w-screen fixed bottom-0 h-min px-3">
         <nav className="relative w-full z-50 p-5 bg-accent-2/25 backdrop-blur-lg max-w-2xl bottom-2 rounded-2xl">
           <ul className="flex flex-row gap-6 items-center justify-between px-3">
-            <Link className= "bg-inherit border-inherit border px-3 py-1 rounded-xl " href={`/`}>Home</Link> 
-          <Link href={`/`} className="text-3xl lg:px-6 font-thin ">DEW</Link>
-            <Link className="border-inherit border px-3 py-1 rounded-xl" href={`/Blog`}>Blog</Link>
+            <Link className= "font-bold hover:underline px-3 py-1 rounded-xl " href={`/`}>Home</Link> 
+          <Link href={`/`} className="text-3xl lg:px-6 font-normal ">DEW</Link>
+            <Link className="font-bold hover:underline px-3 py-1 rounded-xl" href={`/Blog`}>Blog</Link>
           </ul>
         </nav>
     
