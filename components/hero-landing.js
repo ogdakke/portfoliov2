8a1c9ba2d0eb84c66db6fa3fb348dcefdec497a0
ogdakke <daniel.wargh1@gmail.com>
2022-12-30 23:54:29 +0200

    tweaking the manifest, changes in landing page.

--- components/hero-landing.js
+++ components/hero-landing.js
@@ -2,14 +2,14 @@ export default function HeroLanding() {
     return (
     <>
         <h3 className="text-3xl pb-10">
-        I'm a aspiring developer and designer from Finland 🇫🇮 with a passion for beautiful, fast and simple websites. 
+        I'm a aspiring web developer and designer from Finland 🇫🇮 with a passion for beautiful, fast and simple websites. <br/> I create visually pleasing experiences with a precise touch. 
         </h3>
 
-        <h3 className="text-2xl"> Currently doing:</h3> <h4 className="text-2xl text-accent-4 leading-relaxed" >🏫University: Studying Information Systems.</h4>
-        <h4 className="text-2xl text-accent-4" >🧠Learning: React, backend development.</h4>
+        <h4 className="text-xl"> Currently doing:</h4> <h4 className="text-xl text-accent-4 leading-relaxed" >🏫University: Studying Information Systems.</h4>
+        <h4 className="text-xl text-accent-4" >🧠Learning: React, backend development.</h4>
 
-        <div className="py-8 flex justify-center align-middle animate-pulse-slow">
-        <svg width="48" height="48" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <div className="pt-8 flex justify-center align-middle animate-pulse-slow">
+        <svg aria-label="arrows pointing downwards" width="48" height="48" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
         <path d="M38 26L24 40L10 26M38 10L24 24L10 10" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
         </svg>
         </div>
